a7f4d6985ad6f57bf5d1b3e3784696db257a87e3
AndreaDenina <andredeninaf@gmail.com>
2025-06-01 15:32:31 +0200

    modified primary_language script

--- scripts/primary_language.py
+++ scripts/primary_language.py
@@ -2,13 +2,15 @@ from scripts import config
 import os
 import sqlite3
 import csv
+
+import pandas as pd
+
 import logging
 
 logger = logging.getLogger(__name__)
 
-def cross_wiki_editor_metrics(wikilanguagecodes):
 
-    
+def cross_wiki_editor_metrics(wikilanguagecodes):
 
     conn = sqlite3.connect(config.databases_path + 'vital_signs_editors.db')
     cursor = conn.cursor()
@@ -22,7 +24,7 @@ def cross_wiki_editor_metrics(wikilanguagecodes):
                 languagecode+'wiki_editors WHERE user_name != "";'
         cursor.execute(query)
         conn.commit()
-        
+
     try:
         os.remove(config.databases_path + 'temporary_editor_metrics.txt')
     except:
@@ -31,17 +33,20 @@ def cross_wiki_editor_metrics(wikilanguagecodes):
 
     query = 'SELECT user_name, lang, edit_count, year_month_first_edit, lustrum_first_edit FROM allwiki_editors ORDER BY user_name, edit_count DESC;'
 
-    numbereditors = 0
-    totallangs_ecount = 0
-    numberlangs = 0
-    primarylang = ''
-    primary_ecount = 0
-    primary_year_month_first_edit = ''
-    primary_lustrum_first_edit = ''
+    columns = [
+        "user_name",          # string
+        "prim_lang",          # string (primary language)
+        "prim_ecount",        # int (number of edits in primary language)
+        "tot_ecount",        # int (total edit count)
+        "n_langs",           # int (number of languages used)
+        "prim_ym_first_e",   # ym first edit primary lang
+        "prim_lus_first_e"   # lustrum first edit prim
+    ]
 
-    old_user_name = ''
+    df = pd.DataFrame(columns=columns)
 
     for row in cursor.execute(query):
+
         user_name = row[0]
         lang = row[1]
         try:
@@ -59,46 +64,37 @@ def cross_wiki_editor_metrics(wikilanguagecodes):
         except:
             lustrum_first_edit = ''
 
-        if user_name != old_user_name and old_user_name != '':
-            numbereditors += 1
+        if user_name in df['user_name'].values:
+            if edit_count > 4 and lang != "meta":
+                df.loc[df['user_name'] == user_name, 'n_langs'] += 1
 
-            try:
-                edfile2.write(primarylang+'\t'+str(primary_ecount)+'\t'+str(totallangs_ecount)+'\t'+str(numberlangs) +
-                              '\t' + primary_year_month_first_edit+'\t'+primary_lustrum_first_edit+'\t'+old_user_name+'\n')
-            except:
-                pass
+            if (edit_count >= df.loc[df['user_name'] == user_name, 'prim_ecount'] and lang != "meta"):
+                df.loc[df['user_name'] == user_name, 'prim_lang'] = lang
+                df.loc[df['user_name'] == user_name, 'prim_ecount'] = edit_count
 
-            # clean
-            totallangs_ecount = 0
-            numberlangs = 0
-            primarylang = ''
-            primary_ecount = 0
-            primary_year_month_first_edit = ''
-            primary_lustrum_first_edit = ''
+                df.loc[df['user_name'] == user_name,
+                       'prim_ym_first_e'] = year_month_first_edit
+                df.loc[df['user_name'] == user_name,
+                       'prim_lus_first_e'] = lustrum_first_edit
 
-        if edit_count > 4 and lang != "meta":
-            numberlangs += 1
+            df.loc[df['user_name'] == user_name, 'tot_ecount'] += edit_count
+        else:
+            new_row = {
+                'user_name': user_name,
+                'prim_lang': lang,
+                'prim_ecount': edit_count,
+                'tot_ecount': edit_count,
+                'n_langs': 1,
+                'prim_ym_first_e': year_month_first_edit,
+                'prim_lus_first_e': lustrum_first_edit
+            }
+            df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
 
-        # by definition we do not consider that meta can be a primary language. it is not a wikipedia.
-        if (edit_count > primary_ecount and lang != "meta"):
-            primarylang = lang
-            primary_ecount = edit_count
-
-            primary_year_month_first_edit = year_month_first_edit
-            primary_lustrum_first_edit = lustrum_first_edit
-
-        if primarylang == '' and lang != "meta":
-            primarylang = lang
-
-        totallangs_ecount += edit_count
-
-        old_user_name = user_name
-        old_lang = lang
-
-    edfile2.write(primarylang+'\t'+str(primary_ecount)+'\t'+str(totallangs_ecount)+'\t'+str(numberlangs) +
-                  '\t' + primary_year_month_first_edit+'\t'+primary_lustrum_first_edit+'\t'+user_name+'\n')
-
-
+    for row in df.itertuples(index=False):
+        edfile2.write(
+            f"{row.prim_lang}\t{row.prim_ecount}\t{row.tot_ecount}\t{row.n_langs}\t"
+            f"{row.prim_ym_first_e}\t{row.prim_lus_first_e}\t{row.user_name}\n"
+        )
 
     query = "DROP TABLE allwiki_editors;"
     cursor.execute(query)
@@ -121,10 +117,7 @@ def cross_wiki_editor_metrics(wikilanguagecodes):
 
     logger.info("Calculated cross wiki metrics")
 
-
     try:
         os.remove(config.databases_path + 'temporary_editor_metrics.txt')
     except:
         pass
-
-   
