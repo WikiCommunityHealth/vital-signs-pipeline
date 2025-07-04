79e5892ce9e0de913875123cf00c0669649a6e61
AndreaDenina <andredeninaf@gmail.com>
2025-07-04 15:53:23 +0200

    preparing for deployment

--- scripts/config.py
+++ scripts/config.py
@@ -3,7 +3,7 @@ databases_path = '/opt/airflow/databases/'
 vital_signs_editors_db = 'vital_signs_editors.db'
 vital_signs_web_db = 'vital_signs_web.db'
 # '/home/pata/Scrivania/tirocinio/airflow/mediawiki_history_dumps'
-dumps_path = '/mnt/nfs/dumps-clouddumps1001.wikimedia.org/other/mediawiki_history/'
+dumps_path = '/opt/airflow/mediawiki_history'
 
 # /mnt/nfs/dumps-clouddumps1001.wikimedia.org/other/mediawiki_history/
 
