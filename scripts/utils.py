9490bea4ee800649ce63e334b4ba1f393b7ec902
AndreaDenina <andredeninaf@gmail.com>
2025-07-19 15:23:03 +0200

    fixing utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -9,37 +9,41 @@ def get_mediawiki_paths(languagecode):
     year_month = (datetime.date.today() -
                   relativedelta.relativedelta(months=1)).strftime('%Y-%m')
 
-    if not os.path.exists(config.dumps_path + f"/{year_month}"):
+    path = os.path.join(config.dumps_path, year_month)
+
+    if not os.path.exists(path):
         year_month = (datetime.date.today() -
                       relativedelta.relativedelta(months=2)).strftime('%Y-%m')
+        path = os.path.join(config.dumps_path, year_month)
 
-    path = config.dumps_path + f"/{year_month}"
-
-    dir_name = languagecode + "wiki/"
+    dir_name = languagecode + "wiki"
+    path_dir = os.path.join(path, dir)
     cy = datetime.datetime.now().year
     d_paths = []
 
-
     file_name = f'{year_month}.{languagecode}wiki.all-time.tsv.bz2'
-    abs_path = path + dir_name + file_name
-    
+    abs_path = os.path.join(path_dir, file_name)
+
     if os.path.isfile(abs_path):
         d_paths.append(abs_path)
     else:
-        for year in range(2001, cy+1):
-            file_name = f'{year_month}.{languagecode}wiki.{year}.tsv.bz2'
-            abs_path = path + dir_name + file_name
-            
-            if os.path.isfile(abs_path):
-                d_paths.append(abs_path)
-                
-        for year in range(2001, cy+1):
-            for month in range(1, 13):
-                file_name = f'{year_month}.{languagecode}wiki.{year}-{month:02d}.tsv.bz2'
+
+        if languagecode == 'en':
+            for year in range(2001, cy+1):
+                for month in range(1, 13):
+                    file_name = f'{year_month}.{languagecode}wiki.{year}-{month:02d}.tsv.bz2'
+                    abs_path = path + dir_name + file_name
+
+                    if os.path.isfile(abs_path):
+                        d_paths.append(abs_path)
+        else:
+            for year in range(2001, cy+1):
+                file_name = f'{year_month}.{languagecode}wiki.{year}.tsv.bz2'
                 abs_path = path + dir_name + file_name
-                
+
                 if os.path.isfile(abs_path):
                     d_paths.append(abs_path)
+
     print(d_paths)
 
     return d_paths, year_month
