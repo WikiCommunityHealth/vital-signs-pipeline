4bef1f695ab02e003df64ae542bf923333fb4609
AndreaDenina <andredeninaf@gmail.com>
2025-07-19 14:52:50 +0200

    fixing utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -19,26 +19,20 @@ def get_mediawiki_paths(languagecode):
     cy = datetime.datetime.now().year
     d_paths = []
 
-    yearly = False
-    all_time = False
 
     file_name = f'{year_month}.{languagecode}wiki.all-time.tsv.bz2'
     abs_path = path + dir_name + file_name
     
     if os.path.isfile(abs_path):
         d_paths.append(abs_path)
-        all_time = True
-
-    if not all_time:
+    else:
         for year in range(2001, cy+1):
             file_name = f'{year_month}.{languagecode}wiki.{year}.tsv.bz2'
             abs_path = path + dir_name + file_name
             
             if os.path.isfile(abs_path):
                 d_paths.append(abs_path)
-                yearly = True
-
-    if not yearly or not all_time:
+                
         for year in range(2001, cy+1):
             for month in range(1, 13):
                 file_name = f'{year_month}.{languagecode}wiki.{year}-{month:02d}.tsv.bz2'
